792381f91e242e8c6e034215accd1e9a8e4fc6f5
zed-chi <zed-chi@mail.ru>
2023-02-09 22:03:24 +0300

    added file db for test

---
+++ db/memory.py
@@ -0,0 +1,32 @@
+class DB:
+    def __init__(self) -> None:
+        self.db = {
+            "users":[],
+            "packages":[]
+        }
+
+    def add(self, table, value):
+        self.db[table].append(value)
+
+    def update(self, table, pk, key, value):
+        for idx, elem in enumerate(self.db[table]):
+            if elem["pk"] == pk:
+                self.db[table][idx][key] = value
+
+    def delete(self, table:str, pk:str):
+        for idx, elem in enumerate(self.db[table]):
+            if elem["pk"] == pk:
+                del(self.db[table][idx])
+    
+    def get(self, table:str, pk:str):
+        for idx, elem in enumerate(self.db[table]):
+            if elem["pk"] == pk:
+                return elem
+    
+    def count(self, table):
+        if not table in self.db:
+            return 
+        return len(self.db[table])
+
+
+db = DB()
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from environs import Env
 from starlette.templating import Jinja2Templates
+from db import memory
 
 
 env = Env()
@@ -9,3 +10,4 @@ HTML_DIR = env.str("HTML_DIR", "templates")
 ASSETS_DIR = env.str("ASSETS_DIR", "assets")
 
 template_manager = Jinja2Templates(directory=HTML_DIR)
+db = memory.db
