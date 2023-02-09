a0ddd5c34732b25e99ef8e4bfa9fbaa854bac682
zed-chi <zed-chi@mail.ru>
2023-02-09 22:02:43 +0300

    lint

--- views/home.py
+++ views/home.py
@@ -1,29 +1,29 @@
-from fastapi import APIRouter, Request 
+from fastapi import APIRouter, Request
+
 from settings import template_manager
 from view_data.home import IndexViewData
 
-
 home_router = APIRouter()
+
+
 @home_router.get("/")
-def index(request:Request):
+def index(request: Request):
     """Index page."""
     view_data = IndexViewData(request)
-    context = {"request":request}
+    context = {"request": request}
     context.update(view_data.to_dict())
 
-    return template_manager.TemplateResponse(        
-        "./home/index.html",
-        context=context
+    return template_manager.TemplateResponse(
+        "./home/index.html", context=context
     )
 
 
 @home_router.get("/about")
-def about(request:Request):
+def about(request: Request):
     """About page."""
     view_data = IndexViewData(request)
-    context = {"request":request}
+    context = {"request": request}
     context.update(view_data.to_dict())
-    return template_manager.TemplateResponse(        
-        "./home/about.html",
-        context=context
+    return template_manager.TemplateResponse(
+        "./home/about.html", context=context
     )
