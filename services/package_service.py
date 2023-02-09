6cdac498899947558c5f28f099408faf0af3f5d6
zed-chi <zed-chi@mail.ru>
2023-02-10 00:25:01 +0300

    added two getters

--- services/package_service.py
+++ services/package_service.py
@@ -1,10 +1,22 @@
 def release_count():
     return 2
 
+
 def package_count():
     return 1
 
+
 def latest_releases(limit=5):
     return [
-        {"id":1, "name":"qwe", "description":"test packege for testing"}
+        {"id": 1, "name": "qwe", "description": "test packege for testing"}
     ]
+
+
+def get_package(pk=None):
+    if not pk:
+        return None
+    return None
+
+
+def get_latest_release(package):
+    pass
