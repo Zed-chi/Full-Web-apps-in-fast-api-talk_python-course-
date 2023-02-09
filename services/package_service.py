e12b7bf0c2a6c7132aa39a060c1cf62440556885
zed-chi <zed-chi@mail.ru>
2023-02-09 15:57:03 +0300

    init services

---
+++ services/package_service.py
@@ -0,0 +1,10 @@
+def release_count():
+    return 2
+
+def package_count():
+    return 1
+
+def latest_releases(limit=5):
+    return [
+        {"id":1, "name":"qwe", "description":"test packege for testing"}
+    ]
