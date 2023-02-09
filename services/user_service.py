d3b30293d05a2bd8c6651f9bd34244198eb4d448
zed-chi <zed-chi@mail.ru>
2023-02-10 00:25:34 +0300

    implement some user logic

--- services/user_service.py
+++ services/user_service.py
@@ -1,2 +1,29 @@
+from typing import Optional
+
+from models import account
+from settings import db
+
+
 def user_count():
-    return 123
+    return db.count("users")
+
+
+def get_user(pk=None):
+    if not pk:
+        return
+    db.get("users", pk=pk)
+
+
+def create_user(name, email, password):
+    user = account.User(name=name, email=email, hashed_pass=password)
+    db.add("users", user.to_dict())
+    return user
+
+
+def login_user(email, password):
+    user: Optional[dict] = db.get("users", email)
+    print(f"=== {user} ===")
+    if not user:
+        return
+    if user["password"] == password:
+        return user
