e318b55365ef09d56932611fcb3fd84d7310f340
zed-chi <zed-chi@mail.ru>
2023-02-10 16:08:20 +0300

    lint

--- db/memory.py
+++ db/memory.py
@@ -1,9 +1,9 @@
+from typing import Optional
+
+
 class DB:
-    def __init__(self) -> None:
-        self.db = {
-            "users":[],
-            "packages":[]
-        }
+    def __init__(self):
+        self.db = {"users": [], "packages": []}
 
     def add(self, table, value):
         self.db[table].append(value)
@@ -13,19 +13,19 @@ class DB:
             if elem["pk"] == pk:
                 self.db[table][idx][key] = value
 
-    def delete(self, table:str, pk:str):
+    def delete(self, table: str, pk: str):
         for idx, elem in enumerate(self.db[table]):
             if elem["pk"] == pk:
-                del(self.db[table][idx])
-    
-    def get(self, table:str, pk:str):
-        for idx, elem in enumerate(self.db[table]):
+                del self.db[table][idx]
+
+    def get(self, table: str, pk: str) -> Optional[dict]:
+        for elem in self.db[table]:
             if elem["pk"] == pk:
                 return elem
-    
+
     def count(self, table):
-        if not table in self.db:
-            return 
+        if table not in self.db:
+            return
         return len(self.db[table])
 
 
