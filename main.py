52b3f0d6274d0b754cb2323ec110e9c2a777e4d5
zed-chi <zed-chi@mail.ru>
2023-02-10 21:08:22 +0300

    added db init

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from starlette.staticfiles import StaticFiles
 
 from settings import ASSETS_DIR
 from views import account, home, packages
+from sqlalchemy_stuff.db_session import global_init
 
 app = fastapi.FastAPI()
 
@@ -12,12 +13,17 @@ def main():
     init()
     uvicorn.run(app)
 
+def setup_db():
+    db_file = "./data.db"
+    global_init(db_file)
+    
 
 def init():
     app.mount("/static", app=StaticFiles(directory=ASSETS_DIR))
     app.include_router(account.account_router)
     app.include_router(home.home_router)
     app.include_router(packages.packages_router)
+    setup_db()
 
 
 if __name__ == "__main__":
