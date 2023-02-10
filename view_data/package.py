8c0da16a1a60f01163971ea12cc33f5eb73b1eb5
zed-chi <zed-chi@mail.ru>
2023-02-10 21:07:24 +0300

    cleaned class

--- view_data/package.py
+++ view_data/package.py
@@ -1,15 +1,13 @@
-from view_data.base import BaseViewData
 from services import package_service, user_service
+from view_data.base import BaseViewData
 
 
 class DetailViewData(BaseViewData):
-    def __init__(self, request):
+    def __init__(self, p_id, request):
         super().__init__(request)
-
-        self.package_count = package_service.package_count()
-        self.user_count = user_service.user_count()
-        self.release_count = package_service.release_count()
-        self.latest_releases = package_service.latest_releases()
+        self.package = package_service.get_package(pk=p_id)
+        if not self.package:
+            return None
 
 
 class ListViewData(BaseViewData):
