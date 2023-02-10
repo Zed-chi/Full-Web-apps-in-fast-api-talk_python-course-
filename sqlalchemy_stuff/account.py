5af813b010683bab2cd2b7ca17f12e20e535dfa0
zed-chi <zed-chi@mail.ru>
2023-02-11 00:31:55 +0300

    field fixes

--- sqlalchemy_stuff/account.py
+++ sqlalchemy_stuff/account.py
@@ -1,18 +1,35 @@
 from datetime import datetime
 
 import sqlalchemy as sql
+import sqlalchemy.orm as orm
 
 from .base import SQLAlchemyBase
 
 
 class User(SQLAlchemyBase):
-    __tablename__ = "accounts"
-    id = sql.Column(sql.String, primary_key=True, unique=True)
+    __tablename__ = "users"
+    id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
     name = sql.Column(sql.String)
     email = sql.Column(sql.String, unique=True)
     password = sql.Column(sql.String)
     last_login = sql.Column(sql.DateTime, default=datetime.now())
     created_at = sql.Column(sql.DateTime, default=datetime.now())
 
+    package_id = sql.Column(
+        sql.Integer, sql.ForeignKey("packages.id"), nullable=True
+    )
+    package = orm.relationship("Package", back_populates="author")
+    maintained_packages = orm.relationship(
+        "Package", secondary="user_maintain", back_populates="maintainers"
+    )
+
     def __repr__(self) -> str:
         return f"Package {self.id}"
+
+
+class UserMaintains(SQLAlchemyBase):
+    __tablename__ = "user_maintain"
+
+    id = sql.Column(sql.Integer, primary_key=True)
+    user_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+    project_id = sql.Column(sql.Integer, sql.ForeignKey("packages.id"))
--- sqlalchemy_stuff/package.py
+++ sqlalchemy_stuff/package.py
@@ -8,9 +8,13 @@ from .base import SQLAlchemyBase
 
 class License(SQLAlchemyBase):
     __tablename__ = "licenses"
-    id = sql.Column(sql.String, primary_key=True, unique=True)
+    id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
     name = sql.Column(sql.String, unique=True)
     description = sql.Column(sql.String)
+    package_id = sql.Column(
+        sql.Integer, sql.ForeignKey("packages.id"), nullable=True
+    )
+    package = orm.relationship("Package")
 
     def __repr__(self) -> str:
         return f"License {self.id}"
@@ -18,7 +22,7 @@ class License(SQLAlchemyBase):
 
 class Release(SQLAlchemyBase):
     __tablename__ = "releases"
-    id = sql.Column(sql.String, primary_key=True, unique=True)
+    id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
     maj = sql.Column(sql.Integer, default=0)
     min = sql.Column(sql.Integer, default=0)
     build = sql.Column(sql.Integer, default=0)
@@ -64,7 +68,9 @@ class Package(SQLAlchemyBase):
     description = sql.Column(sql.Text, nullable=True)
 
     author = orm.relationship("User", back_populates="package")
-    maintainers = orm.relationship("User", back_populates="package")
+    maintainers = orm.relationship(
+        "User", secondary="user_maintain", back_populates="maintained_packages"
+    )
 
     home_page_url = sql.Column(sql.String, nullable=True)
     docs_url = sql.Column(sql.String, nullable=True)
