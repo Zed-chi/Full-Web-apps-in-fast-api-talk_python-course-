050b1c12b2042a95c00176aebfd0414f074d59c9
zed-chi <zed-chi@mail.ru>
2023-02-10 16:13:44 +0300

    injected data to views, to share common auth vars

--- views/packages.py
+++ views/packages.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 
 from settings import template_manager
+from view_data.package import DetailViewData, ListViewData
 
 packages_router = APIRouter(prefix="/packages")
 
@@ -8,14 +9,20 @@ packages_router = APIRouter(prefix="/packages")
 @packages_router.get("/")
 def list_view(request: Request):
     """Index page."""
+    view_data = ListViewData(request)
+    context = {"request": request}
+    context.update(view_data.to_dict())
     return template_manager.TemplateResponse(
-        "./packages/list.html", context={"request": request}
+        "./packages/list.html", context=context
     )
 
 
 @packages_router.get("/{p_id}")
 def detail_view(p_id: str, request: Request):
     """Index page."""
+    view_data = DetailViewData(p_id, request)
+    context = {"request": request}
+    context.update(view_data.to_dict())    
     return template_manager.TemplateResponse(
-        "./packages/detail.html", context={"request": request}
+        "./packages/detail.html", context=context
     )
