804c1480deaffb913b57169530c4d4ceedbaff58
zed-chi <zed-chi@mail.ru>
2023-02-10 16:09:36 +0300

    implemented view_data for account logic

--- view_data/account.py
+++ view_data/account.py
@@ -1,32 +1,94 @@
+from services import user_service
 from view_data.base import BaseViewData
-from services import package_service, user_service
 
 
 class AccountViewData(BaseViewData):
     def __init__(self, request):
         super().__init__(request)
 
-        self.package_count = package_service.package_count()
-        self.user_count = user_service.user_count()
-        self.release_count = package_service.release_count()
-        self.latest_releases = package_service.latest_releases()
-
 
 class AccountRegisterViewData(BaseViewData):
     def __init__(self, request):
         super().__init__(request)
 
-        self.package_count = package_service.package_count()
-        self.user_count = user_service.user_count()
-        self.release_count = package_service.release_count()
-        self.latest_releases = package_service.latest_releases()
+    async def load(self):
+        form = await self.request.form()
+        validated = self.validate_form(form)
+        self.name = validated["name"]
+        self.email = validated["email"]
+        self.password = hash(validated["password"])
+
+    def validate_name(self, name):
+        if not name:
+            raise ValueError("not name")
+        name = self.sanitize(name)
+        if len(name) < 3:
+            raise ValueError("Length must be >=8")
+        return self.sanitize(name)
+
+    def validate_email(self, email):
+        if user_service.get_user(pk=email):
+            raise ValueError("User exists")
+        return self.sanitize(email)
+
+    def validate_pass(self, password):
+        if len(password) < 4:
+            raise ValueError("length < 8 chars")
+        return self.sanitize(password)
+
+    def validate_form(self, form):
+        name = form.get("name")
+        password = form.get("password")
+        email = form.get("email")
+        return {
+            "name": self.validate_name(name),
+            "email": self.validate_email(email),
+            "password": self.validate_pass(password),
+        }
+
+    def sanitize(self, name: str):
+        for i in "~!#$%^&*()_ -=+:;'\"\r\t\n":
+            name = name.replace(i, "_")
+        return name
 
 
 class AccountLoginViewData(BaseViewData):
     def __init__(self, request):
         super().__init__(request)
 
-        self.package_count = package_service.package_count()
-        self.user_count = user_service.user_count()
-        self.release_count = package_service.release_count()
-        self.latest_releases = package_service.latest_releases()
+    async def load(self):
+        form = await self.request.form()
+        validated = self.validate_form(form)
+        self.email = validated["email"]
+        self.password = hash(validated["password"])
+
+    def validate_name(self, name):
+        if not name:
+            raise ValueError("not name")
+        name = self.sanitize(name)
+        if len(name) < 3:
+            raise ValueError("Length must be >=8")
+        return self.sanitize(name)
+
+    def validate_email(self, email):
+        if user_service.get_user(pk=email):
+            raise ValueError("User exists")
+        return self.sanitize(email)
+
+    def validate_pass(self, password):
+        if len(password) < 4:
+            raise ValueError("length < 8 chars")
+        return self.sanitize(password)
+
+    def validate_form(self, form):
+        password = form.get("password")
+        email = form.get("email")
+        return {
+            "email": self.validate_email(email),
+            "password": self.validate_pass(password),
+        }
+
+    def sanitize(self, name: str):
+        for i in "~!#$%^&*()_ -=+:;'\"\r\t\n":
+            name = name.replace(i, "_")
+        return name
