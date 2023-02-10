03c6e5e97257b6d98de45ce631eb06dbaf605658
zed-chi <zed-chi@mail.ru>
2023-02-10 16:11:58 +0300

    changed auth attrs

--- view_data/base.py
+++ view_data/base.py
@@ -1,8 +1,15 @@
+from infrastructure import cookie_auth
+
+
 class BaseViewData:
     def __init__(self, request) -> None:
         self.request = request
         self.error = None
-        self.user_id = None
-    
+        self.user_id = cookie_auth.get_user_id_from_auth_cookie(
+            self.request
+        )
+        self.is_logged_in = self.user_id is not None
+        self.error = ""
+
     def to_dict(self):
         return self.__dict__
